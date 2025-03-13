025366e80d80900e769f1bc433f52779a0086b31
ERRAZI <errazi111@gmail.com>
2025-03-13 03:09:01 +0000

    added gameitem, gameslist and navbar components

---
+++ src/app/components/games-list/games-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games-list',
+  imports: [],
+  templateUrl: './games-list.component.html',
+  styleUrl: './games-list.component.scss'
+})
+export class GamesListComponent {
+
+}
