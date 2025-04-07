08ac8466ed6b3a0a9b617994c0e2e453150e0b22
ERRAZI <errazi111@gmail.com>
2025-04-07 21:29:16 +0100

    added animations to worduess game + improve game functionality

--- src/app/modules/games/worduess/worduess/worduess.component.ts
+++ src/app/modules/games/worduess/worduess/worduess.component.ts
@@ -8,16 +8,46 @@ import { Component, HostListener } from '@angular/core';
   styleUrl: './worduess.component.scss'
 })
 export class WorduessComponent {
+  wordsList: string[] = ["ALBUM",
+    "GIANT",
+    "GREAT",
+    "STATE",
+    "WORLD",
+    "START",
+    "LOSER",
+    "READY",
+    "SORRY",
+    "ZEBRA",
+    "TIGER",
+    "RIVER",
+    "CYCLE",
+    "ABOUT",
+    "PRIZE",
+    "ADAPT",
+    "BLOCK",
+    "HUMOR"];
+  destroyAttempt = [false, false, false, false, false];
   attempts: string[][];
   attemptsValidation: number[][];
   showInstructions = false;
-  currentWord = '';
+  currentWord = '     ';
   currentAttempt = 0;
+  currentIndex = 0;
   startGame = false;
 
-  wordToGuess = this.getRandomWord();
+  wordToGuess = '';
+
+  gameSolved = false;
 
   constructor() {
+    this.attempts = [];
+    this.attemptsValidation = [];
+    this.constructGame();
+  }
+
+  constructGame() {
+    this.wordToGuess = this.getRandomWord();
+    console.log("correct word: "+this.wordToGuess);
     this.attempts = [];
     this.attemptsValidation = [];
 
@@ -29,29 +59,50 @@ export class WorduessComponent {
             this.attemptsValidation[i][j] = 0;
         }
     }
+
   }
 
 
   @HostListener('window:keyup', ['$event'])
   keyEvent(event: KeyboardEvent) {
+    if(this.gameSolved) return;
     if(event.keyCode<=90 && event.keyCode>=65){
-      if (this.currentWord.length==5) return;
-      this.attempts[this.currentAttempt][this.currentWord.length] = event.key;
-      this.currentWord = this.currentWord + event.key;
+      if (this.currentIndex==5) return;
+      this.attempts[this.currentAttempt][this.currentIndex] = event.key;
+      this.currentWord = this.currentWord.substring(0, this.currentIndex) + event.key.toUpperCase() + this.currentWord.substring(this.currentIndex+1, 5);
+      this.currentIndex++;
+      while(this.currentIndex<5 && this.attemptsValidation[this.currentAttempt][this.currentIndex]==1) this.currentIndex++;
     } else if (event.key==='Backspace') {
-      this.attempts[this.currentAttempt][this.currentWord.length-1] = '';
-      this.currentWord = this.currentWord.substring(0, this.currentWord.length-1);
-      console.log("remove last letter");
-    } else if (this.currentWord.length==5 && event.key==='Enter') {
-      console.log("check attempted word");
-      this.currentAttempt++;
-      this.currentWord = '';
+      this.deleteLastEnteredKey();
+    } else if (this.currentIndex==5 && event.key==='Enter') {
+      this.checkAttempt();
     }
+  }
+
+  checkAttempt() {
+    console.log("check attempted word: "+this.currentWord);
+    if (this.currentWord === this.wordToGuess.toUpperCase()) {
+      this.attemptsValidation[this.currentAttempt]=[1,1,1,1,1];
+      console.log("CONGRATULATIONS YOU SOLVED IT! :)");
+      this.gameSolved=true;
+      for(let i =this.currentAttempt+1; i<6; i++) {
+        this.destroyAttempt[i]=true;
+      }
+    }
+    this.currentAttempt++;
+    this.currentWord = '     ';
+    this.currentIndex=0;
+  }
+
+  deleteLastEnteredKey() {
+    this.attempts[this.currentAttempt][this.currentWord.length-1] = '';
+    this.currentWord = this.currentWord.substring(0, this.currentWord.length-1);
+    console.log("remove last letter");
   }
 
 
   getRandomWord() {
-    return 'brain';
+    return this.wordsList[Math.floor(Math.random()*this.wordsList.length)];
   }
 
   showInstructionsList() {
