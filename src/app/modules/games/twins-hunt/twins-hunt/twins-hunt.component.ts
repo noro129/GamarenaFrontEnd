c9f0962a815b20652c30356e8ef7dea9e07bf121
ERRAZI <errazi111@gmail.com>
2025-04-13 20:35:37 +0100

    implemented twins hunt game basic functionality

--- src/app/modules/games/twins-hunt/twins-hunt/twins-hunt.component.ts
+++ src/app/modules/games/twins-hunt/twins-hunt/twins-hunt.component.ts
@@ -1,11 +1,99 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { TimerComponent } from '../../../../components/timer/timer.component';
+import { GameToolbarComponent } from '../../../../components/game-toolbar/game-toolbar.component';
+import { GameResultComponent } from '../../../../components/game-result/game-result.component';
 
 @Component({
   selector: 'app-twins-hunt',
-  imports: [],
+  imports: [NgFor, TimerComponent, GameToolbarComponent, GameResultComponent],
   templateUrl: './twins-hunt.component.html',
   styleUrl: './twins-hunt.component.scss'
 })
-export class TwinsHuntComponent {
+export class TwinsHuntComponent{
+  @ViewChild('matrix') matrix!: ElementRef;
 
+
+
+  colorsMatrix : string[][] = [
+  ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"],
+  ["#FF8000", "#8000FF", "#750955", "#00FF80", "#FF0080", "#808080"],
+  ["#804000", "#FF8080", "#FFFF80", "#80FF80", "#8080FF", "#000000"],
+  ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"],
+  ["#FF8000", "#8000FF", "#750955", "#00FF80", "#FF0080", "#808080"],
+  ["#804000", "#FF8080", "#FFFF80", "#80FF80", "#8080FF", "#000000"]
+  ];
+  rows = 6;
+  columns = 6;
+  startGame = false;
+  gameSolved = false;
+  showCells = false;
+  colorsMatched = 0;
+  colorTwin1 = [-1, -1];
+  colorTwin2 = [-1, -1];
+
+  constructor(private renderer : Renderer2) {}
+
+  
+  handleGameStart(event : boolean) {
+    const cells = this.matrix.nativeElement.querySelectorAll(".row-cell");
+    this.startRevealing(cells, 0);
+  }
+
+  startRevealing(elements : ElementRef[], index : number) {
+    setTimeout(()=>{
+      let rowIndex = Math.floor(index/this.rows);
+      let columnIndex = index - rowIndex*this.rows;
+      this.renderer.setStyle(elements[index], 'background-color', this.colorsMatrix[rowIndex][columnIndex])
+      this.renderer.addClass(elements[index], 'show-cell');
+      if (index<elements.length-1) this.startRevealing(elements, index+1);
+      else this.startHiding(elements, 0);
+    },300)
+  }
+
+  startHiding(elements : ElementRef[], index : number) {
+    setTimeout(()=>{
+      this.renderer.removeClass(elements[index], 'show-cell');
+      if (index < elements.length - 1) this.startHiding(elements, index+1);
+      else this.startGame = true;
+    },300)
+  }
+
+  indexesArray(size : number) : number[] {
+    return Array.from({ length : size}, (_, i) => i);
+  }
+
+  cellClick(i: number, j: number) {
+    if(!this.startGame || this.colorTwin2[0] != -1) return;
+    if (this.colorTwin1[0]===-1) {
+      this.colorTwin1 = [i, j];
+      this.renderer.addClass(this.matrix.nativeElement.querySelectorAll(".row-cell")[i*this.rows + j], 'show-cell');
+    } else {
+      this.colorTwin2 = [i, j];
+      this.renderer.addClass(this.matrix.nativeElement.querySelectorAll(".row-cell")[i*this.rows + j], 'show-cell');
+
+      this.areEqual(this.colorTwin1, this.colorTwin2);
+    }
+  }
+
+  areEqual(cell1: number[], cell2: number[]) {
+    const className = this.colorsMatrix[cell1[0]][cell1[1]] === this.colorsMatrix[cell2[0]][cell2[1]] ? "correct-match-animation" : "error-match-animation";
+    this.renderer.addClass(this.matrix.nativeElement.querySelectorAll(".row-cell")[cell1[0]*this.rows + cell1[1]], className);
+    this.renderer.addClass(this.matrix.nativeElement.querySelectorAll(".row-cell")[cell2[0]*this.rows + cell2[1]], className);
+
+
+    setTimeout(()=>{
+
+      if(this.colorsMatrix[cell1[0]][cell1[1]] !== this.colorsMatrix[cell2[0]][cell2[1]]){
+        this.renderer.removeClass(this.matrix.nativeElement.querySelectorAll(".row-cell")[cell1[0]*this.rows + cell1[1]], 'show-cell');
+        this.renderer.removeClass(this.matrix.nativeElement.querySelectorAll(".row-cell")[cell2[0]*this.rows + cell2[1]], 'show-cell');
+      } else {
+        this.colorsMatched++;
+      }
+      this.colorTwin1 = [-1, -1];
+      this.colorTwin2 = [-1, -1];
+      this.renderer.removeClass(this.matrix.nativeElement.querySelectorAll(".row-cell")[cell1[0]*this.rows + cell1[1]], className);
+      this.renderer.removeClass(this.matrix.nativeElement.querySelectorAll(".row-cell")[cell2[0]*this.rows + cell2[1]], className);
+    },350)
+  }
 }
