471ea975fbc82116c8f5aa7bc0c166627bfa83a7
ERRAZI <errazi111@gmail.com>
2025-04-17 03:15:10 +0100

    implemented logout

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,4 +9,11 @@ import { RouterLink } from '@angular/router';
 })
 export class NavBarComponent {
 
+  constructor(private router : Router) {}
+
+
+  logout() {
+    localStorage.removeItem("token");
+    this.router.navigate(["/"]);
+  }
 }
