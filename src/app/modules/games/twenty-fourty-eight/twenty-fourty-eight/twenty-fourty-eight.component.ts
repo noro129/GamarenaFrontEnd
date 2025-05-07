a093c39d3a04013197b2f4e51e8b0d0b8cfd4346
ERRAZI <errazi111@gmail.com>
2025-05-07 02:08:26 +0100

    updated 2048 to use async functions instead of static pushing animation transition time calculation using timeout

--- src/app/modules/games/twenty-fourty-eight/twenty-fourty-eight/twenty-fourty-eight.component.ts
+++ src/app/modules/games/twenty-fourty-eight/twenty-fourty-eight/twenty-fourty-eight.component.ts
@@ -1,6 +1,7 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
 import { GameResultComponent } from "../../../../components/game-result/game-result.component";
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-twenty-fourty-eight',
@@ -20,7 +21,7 @@ export class TwentyFourtyEightComponent {
 
   boardX = 4;
   boardY = 4;
-  board = [[0,0,0,0],[0,0,0,4],[0,0,2,0],[0,2,0,2]]
+  board = [[0,0,0,0],[0,0,0,4],[0,0,2,0],[0,2,0,2]];
 
 
   constructor(private renderer : Renderer2) {}
@@ -30,80 +31,232 @@ export class TwentyFourtyEightComponent {
   }
 
   @HostListener('window:keyup', ['$event'])
-  arrowKey(event : KeyboardEvent) {
-    if(!this.canPush) return;
+  async arrowKey(event : KeyboardEvent) {
+    if(!this.canPush) {
+      console.log('cannot push yet!!!');
+      return;
+    }
+    this.canPush=false;
+    let moved = false;
     if(event.key === 'ArrowUp') {
-      if(this.pushUp()) {
-        this.canPush = false;
-      }
+      moved = await this.pushUp();
     }
-    if(event.key === 'ArrowDown') {
-      if(this.pushDown()) {
-        this.canPush = false;
-      }
+    else if(event.key === 'ArrowDown') {
+      moved = await this.pushDown();
     }
-    if(event.key === 'ArrowLeft') {
-      if(this.pushLeft()) {
-        this.canPush = false;
-      }
+    else if(event.key === 'ArrowLeft') {
+      moved = await this.pushLeft();
     }
-    if(event.key === 'ArrowRight') {
-      if(this.pushRight()) {
-        this.canPush = false;
-      }
+    else if(event.key === 'ArrowRight') {
+      moved = await this.pushRight();
     }
-    setTimeout(()=>{this.canPush = true;this.addNewBlock();}, 300)
+
+    if (moved) {
+      await new Promise(requestAnimationFrame);
+      this.addNewBlock();
+    }
+    this.canPush= true;
   }
 
-  pushUp() : boolean {
+  async pushUp() : Promise<boolean> {
+    let moves = [];
     for(let column=0; column<this.boardY; column++) {
       let blankBlocks = 0;
       for(let row=0; row<this.boardX; row++) {
-        if(this.board[row][column] === 0) blankBlocks++;
-        else this.pushBlockUpBy(row, column, blankBlocks);
+        const value = this.board[row][column];
+        if (value === 0) blankBlocks++;
+        else if (blankBlocks > 0) {
+          moves.push({
+            'row': row,
+            'column': column,
+            'pushBy': blankBlocks
+          })
+        }
       }
-      if (blankBlocks===4 || blankBlocks===0) continue;
     }
+    if(moves.length === 0) return false;
+    let promises = [];
+    for(const move of moves) {
+      const promise = this.pushBlockUpBy(move.row, move.column, move.pushBy);
+      promises.push(promise);
+    }
+    await Promise.all(promises).then(()=>{
+      for(const move of moves){
+        this.pushBoardCellUpBy(move.row, move.column, move.pushBy);
+      }
+    });
     return true;
-  };
+  }
 
-  pushDown() : boolean {
+  async pushDown() : Promise<boolean> {
+    let moves = [];
     for(let column=0; column<this.boardY; column++) {
       let blankBlocks = 0;
       for(let row=this.boardX-1; row>=0; row--) {
-        if(this.board[row][column] === 0) blankBlocks++;
-        else this.pushBlockDownBy(row, column, blankBlocks);
+        const value = this.board[row][column];
+        if(value === 0) blankBlocks++;
+        else if (blankBlocks > 0) {
+          moves.push({
+            'row': row,
+            'column': column,
+            'pushBy': blankBlocks
+          })
+          
+        }
       }
-      if (blankBlocks===4 || blankBlocks===0) continue;
     }
+    if(moves.length === 0) return false;
+    let promises = [];
+    for(const move of moves) {
+      const promise = this.pushBlockDownBy(move.row, move.column, move.pushBy);
+      promises.push(promise);
+    }
+    await Promise.all(promises).then(()=>{
+      for(const move of moves) {
+        this.pushBoardCellDownBy(move.row, move.column, move.pushBy);
+      }
+    });
     return true;
-  };
+  }
 
-  pushLeft() : boolean {
+  async pushLeft() : Promise<boolean> {
+    let moves = [];
     for(let row=0; row<this.boardX; row++) {
       let blankBlocks = 0;
       for(let column=0; column<this.boardY; column++) {
         if(this.board[row][column] === 0) blankBlocks++;
-        else this.pushBlockLeftBy(row, column, blankBlocks);
+        else if (blankBlocks > 0) {
+          moves.push({
+            'row': row,
+            'column': column,
+            'pushBy': blankBlocks
+          })
+          
+        }
       }
-      if (blankBlocks===4 || blankBlocks===0) continue;
+      
     }
+    if(moves.length === 0) return false;
+    let promises = [];
+    for(const move of moves) {
+      const promise = this.pushBlockLeftBy(move.row, move.column, move.pushBy);
+      promises.push(promise);
+    }
+    await Promise.all(promises).then(()=>{
+      for(const move of moves) {
+        this.pushBoardCellLeftBy(move.row, move.column, move.pushBy);
+      }
+    });
     return true;
-  };
+  }
 
-  pushRight() : boolean {
+  async pushRight() : Promise<boolean> {
+    let moves = [];
     for(let row=0; row<this.boardX; row++) {
       let blankBlocks = 0;
       for(let column=this.boardY-1; column>=0; column--) {
-        if(this.board[row][column] === 0) blankBlocks++;
-        else this.pushBlockRightBy(row, column, blankBlocks);
+        if (this.board[row][column] === 0) blankBlocks++;
+        else if (blankBlocks > 0) {
+          moves.push({
+            'row': row,
+            'column': column,
+            'pushBy': blankBlocks
+          })
+          
+        }
       }
-      if (blankBlocks===4 || blankBlocks===0) continue;
     }
+    if(moves.length === 0) return false;
+    let promises = [];
+    for(const move of moves) {
+      const promise = this.pushBlockRightBy(move.row, move.column, move.pushBy);
+      promises.push(promise);
+    }
+    await Promise.all(promises).then(()=>{
+      for(const move of moves) {
+        this.pushBoardCellRightBy(move.row, move.column, move.pushBy);
+      }
+    });
     return true;
-  };
+  }
 
+  ///////////////////////////////// MODIFY THE FOLLOWING METHODS TO ADD MERGING CASES /////////////////////////////////
+
+  async pushBlockUpBy(row : number, column : number, pushBy : number) {
+    if (pushBy === 0 || row<0 || row>=this.boardX || column<0 || column>=this.boardY) return;
+
+    const boardNumber = this.getNumberElement(row, column);
+    this.renderer.setStyle(boardNumber, 'transform', `translateY(${- pushBy * 90}px)`);    
+    await this.waitForTransitionAnimationEnd(boardNumber);
+  }
   
+  async pushBoardCellUpBy(row : number, column : number, pushBy : number) {
+    await new Promise(requestAnimationFrame);
+    const temp = this.board[row][column];
+    this.board[row][column] = 0;
+    this.board[row-pushBy][column] = temp;
+  }
+
+  async pushBlockDownBy(row : number, column : number, pushBy : number) {
+    if (pushBy === 0 || row<0 || row>=this.boardX || column<0 || column>=this.boardY) return;
+    const boardNumber = this.getNumberElement(row, column);
+    this.renderer.setStyle(boardNumber, 'transform', `translateY(${pushBy * 90}px)`);
+
+    await this.waitForTransitionAnimationEnd(boardNumber);
+  }
+
+  async pushBoardCellDownBy(row : number, column : number, pushBy : number) {
+    await new Promise(requestAnimationFrame);
+    const temp = this.board[row][column];
+    this.board[row][column] = 0;
+    this.board[row+pushBy][column] = temp;
+  }
+
+  async pushBlockLeftBy(row : number, column : number, pushBy : number) {
+    if (pushBy === 0 || row<0 || row>=this.boardX || column<0 || column>=this.boardY) return;
+    const boardNumber = this.getNumberElement(row, column);
+    this.renderer.setStyle(boardNumber, 'transform', `translateX(${-pushBy * 90}px)`);
+
+    await this.waitForTransitionAnimationEnd(boardNumber);
+  }
+
+  async pushBoardCellLeftBy(row : number, column : number, pushBy : number) {
+    await new Promise(requestAnimationFrame);
+    const temp = this.board[row][column];
+    this.board[row][column] = 0;
+    this.board[row][column-pushBy] = temp;
+  }
+
+  async pushBlockRightBy(row : number, column : number, pushBy : number) {
+    if (pushBy === 0 || row<0 || row>=this.boardX || column<0 || column>=this.boardY) return;
+
+    const boardNumber = this.getNumberElement(row, column);
+
+    this.renderer.setStyle(boardNumber, 'transform', `translateX(${pushBy * 90}px)`);
+
+    await this.waitForTransitionAnimationEnd(boardNumber);
+  }
+
+  async pushBoardCellRightBy(row : number, column : number, pushBy : number) {
+    await new Promise(requestAnimationFrame);
+    const temp = this.board[row][column];
+    this.board[row][column] = 0;
+    this.board[row][column+pushBy] = temp;
+  }
+
+  //////////////////////////////////////////// CROSS CUTTING CONCERNS:
+  getNumberElement(row : number, column : number) : HTMLElement {
+    let boardRow = this.boardMatrix.nativeElement.querySelectorAll(".row")[row];
+    let boardBlock = boardRow.querySelectorAll(".block")[column];
+    let boardNumber = boardBlock.querySelectorAll(".number");
+    return boardNumber[0];
+  }
+
+  getBlockElement(row: number, column: number) : HTMLElement {
+    let boardRow = this.boardMatrix.nativeElement.querySelectorAll(".row")[row];
+    return boardRow.querySelectorAll(".block")[column];
+  }
+
   addNewBlock() {
     let cells = [];
     for(let row=0; row<this.boardX; row++) {
@@ -119,80 +272,16 @@ export class TwentyFourtyEightComponent {
     this.board[cells[cell][0]][cells[cell][1]] = Math.floor(Math.random() * 10) === 5 ? 4 : 2;
   }
 
-  //////////////////////////////////////// MODIFY THE FOLLOWING METHODS TO ADD MERGING CASES /////////////////////////////////
-
-  pushBlockUpBy(row : number, column : number, pushBy : number) {
-    if (pushBy === 0 || row<0 || row>=this.boardX || column<0 || column>=this.boardY) return;
-
-    const boardNumber = this.getNumberElement(row, column);
-
-    this.renderer.setStyle(boardNumber, 'transform', 'translateY(-'+(pushBy * 90)+'px)');
-    setTimeout(() => {
-      this.pushBoardCellUpBy(row, column, pushBy);
-    }, 200);
-  }
-
-  pushBoardCellUpBy(row : number, column : number, pushBy : number) {
-    const temp = this.board[row][column];
-    this.board[row][column] = 0;
-    this.board[row-pushBy][column] = temp;
-  }
-
-  pushBlockDownBy(row : number, column : number, pushBy : number) {
-    if (pushBy === 0 || row<0 || row>=this.boardX || column<0 || column>=this.boardY) return;
-
-    const boardNumber = this.getNumberElement(row, column);
-    
-    this.renderer.setStyle(boardNumber, 'transform', 'translateY('+(pushBy * 90)+'px)');
-    setTimeout(() => {
-      this.pushBoardCellDownBy(row, column, pushBy);
-    }, 200);
-  }
-
-  pushBoardCellDownBy(row : number, column : number, pushBy : number) {
-    const temp = this.board[row][column];
-    this.board[row][column] = 0;
-    this.board[row+pushBy][column] = temp;
-  }
-
-  pushBlockLeftBy(row : number, column : number, pushBy : number) {
-    if (pushBy === 0 || row<0 || row>=this.boardX || column<0 || column>=this.boardY) return;
-
-    const boardNumber = this.getNumberElement(row, column);
-
-    this.renderer.setStyle(boardNumber, 'transform', 'translateX(-'+(pushBy * 90)+'px)');
-    setTimeout(() => {
-      this.pushBoardCellLeftBy(row, column, pushBy);
-    }, 200);
-  }
-
-  pushBoardCellLeftBy(row : number, column : number, pushBy : number) {
-    const temp = this.board[row][column];
-    this.board[row][column] = 0;
-    this.board[row][column-pushBy] = temp;
-  }
-
-  pushBlockRightBy(row : number, column : number, pushBy : number) {
-    if (pushBy === 0 || row<0 || row>=this.boardX || column<0 || column>=this.boardY) return;
-
-    const boardNumber = this.getNumberElement(row, column);
-
-    this.renderer.setStyle(boardNumber, 'transform', 'translateX('+(pushBy * 90)+'px)');
-    setTimeout(() => {
-      this.pushBoardCellRightBy(row, column, pushBy);
-    }, 200);
-  }
-
-  pushBoardCellRightBy(row : number, column : number, pushBy : number) {
-    const temp = this.board[row][column];
-    this.board[row][column] = 0;
-    this.board[row][column+pushBy] = temp;
-  }
-
-  getNumberElement(row : number, column : number) : ElementRef {
-    let boardRow = this.boardMatrix.nativeElement.querySelectorAll(".row")[row];
-    let boardBlock = boardRow.querySelectorAll(".block")[column];
-    let boardNumber = boardBlock.querySelectorAll(".number");
-    return boardNumber[0];
+  async waitForTransitionAnimationEnd(element : HTMLElement) : Promise<void> {
+    return new Promise(resolve => {
+      const handler = (event : TransitionEvent) => {
+        if (event.target === element) {
+          element.removeEventListener('transitionend', handler);
+          this.renderer.removeStyle(element, 'transform');
+          resolve();
+        }
+      }
+      element.addEventListener('transitionend', handler);
+    })
   }
 }
